43d2acf69319510288551e9fa9c3bea74c946d7b
Takayuki-spz <merge.ff>
2023-03-30 03:19:24 +0900

    UI/UXのブラッシュアップ

--- src/db/seeds.rb
+++ src/db/seeds.rb
@@ -26,18 +26,20 @@ followers.each { |follower| follower.follow(user) }
 
 
 # ユーザーの一部を対象にマイクロポストを生成する
-users = User.order(:created_at).take(6)
-50.times do
-  content = Faker::Lorem.sentence(word_count: 5)
-  users.each { |user| user.microposts.create!(content: content) }
+users = User.order(:created_at).take(90)
+25.times do
+  content = '今日は、Railsのチュートリアルを進めました。MVCアーキテクチャやルーティングの仕組みを理解し、CRUDアプリケーションの作成に挑戦しました!'
+  study_time_hour = rand(1..12)
+  study_time_minute = rand(0..11) * 5
+  users.each { |user| user.microposts.create!(content: content, study_time_hour: study_time_hour, study_time_minute: study_time_minute) }
 end
 
 # マイクロポストの一部を対象にコメントを生成する
-microposts = Micropost.order(:created_at).take(6)
-users = User.order(:created_at).take(6)
+microposts = Micropost.order(:created_at).take(1500)
+users = User.order(:created_at).take(9)
 microposts.each do |micropost|
   users.each do |user|
-    content = Faker::Lorem.sentence(word_count: 15)
+    content = 'Railsのチュートリアルを進めることで、MVCアーキテクチャやルーティングの理解を深め、CRUDアプリケーションの作成に挑戦したのは素晴らしいですね!これからも頑張ってください!'
     micropost.comments.create!(content: content, user: user)
   end
 end
